c9b3b79a7c36860bdd84a6e962f364cb88f11875
Jason <mljasson@gmail.com>
2021-07-21 08:28:08 -0500

    API Back

--- src/config/helpers.js
+++ src/config/helpers.js
@@ -1,4 +1,4 @@
 // export const API = 'http://localhost:3000'
-export const API = 'https://petsfront.herokuapp.com'
+export const API = 'https://petsamelie.herokuapp.com'
 export const token = `bearer ${localStorage.getItem('token')}`
 
